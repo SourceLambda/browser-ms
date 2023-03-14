4e3ddbbdc7a1679eb8008e78492ac15004bd7cf7
bromeroaEvocon <brayan.romero@evolutioncons.com>
2023-03-14 07:26:07 -0500

    Docker IMPL

--- src/main/kotlin/ASHU/dataAccess/RedisConnector.kt
+++ src/main/kotlin/ASHU/dataAccess/RedisConnector.kt
@@ -3,11 +3,15 @@ package ASHU.dataAccess
 import io.github.crackthecodeabhi.kreds.connection.Endpoint
 import io.github.crackthecodeabhi.kreds.connection.KredsClient
 import io.github.crackthecodeabhi.kreds.connection.newClient
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 
 
 //object RedisConnector {
-//    private val client : KredsClient = newClient(Endpoint("redis-17116.c241.us-east-1-4.ec2.cloud.redislabs.com",
-//        17116))
+//    private val client : KredsClient = newClient(
+//        Endpoint("redis-17116.c241.us-east-1-4.ec2.cloud.redislabs.com",
+//        17116)
+//    )
 //
 //    const val DocCountKey = "QG29auKuotaXFB4OFhLekAuAKTVl70dDBGEH8IJQ4jhD4Skoaq1UJsVzpDoBFMtccR7PFsOd1Lkq16E9GINc00AyHReaYaKd5Vf"
 //
